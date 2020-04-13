b9e93e8679ec7c9da2a7e1c3862ca79d02b54b13
Audcart <acarte06@mail.bbk.ac.uk>
2020-04-13 19:12:59 +0200

    Create a01_generalImports.py

---
+++ Memos/a01_generalImports.py
@@ -0,0 +1,18 @@
+# More common
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import datetime
+import os
+import math
+import scipy
+# Less common
+import itertools
+import statsmodels.api as sm
+import statsmodels.stats.stattools as stools
+import statsmodels.stats as stats
+import scipy.stats as scipystats
+from functools import partial
+import random
+
